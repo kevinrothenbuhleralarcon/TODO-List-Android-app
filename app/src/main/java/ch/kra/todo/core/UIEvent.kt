930993af60bef6d540527d548c8632094b7ab7c3
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2022-04-27 16:27:30 +0200

    Remove some UIEvent that are not necesary

--- app/src/main/java/ch/kra/todo/core/UIEvent.kt
+++ app/src/main/java/ch/kra/todo/core/UIEvent.kt
@@ -6,8 +6,6 @@ sealed class UIEvent {
     data class ShowSnackbar(val message: String): UIEvent()
     object PopBackStack: UIEvent()
     data class Navigate(val route: String): UIEvent()
-    data class DisplayError(val error: String): UIEvent()
-    data class DisplaySuccess(val message: String): UIEvent()
     object DisplayLoading: UIEvent()
     data class StartIntent(val intent: Intent): UIEvent()
 }
