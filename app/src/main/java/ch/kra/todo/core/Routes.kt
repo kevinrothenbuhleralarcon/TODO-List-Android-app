8cf3f23b563f7066382be1b9f92bd68145eee4c0
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2022-04-27 13:12:54 +0200

    Add a route for the todoList, add a UIEvent to display a Sucess, an Error or a loading animation

--- app/src/main/java/ch/kra/todo/core/Routes.kt
+++ app/src/main/java/ch/kra/todo/core/Routes.kt
@@ -1,5 +1,6 @@
 package ch.kra.todo.core
 
 object Routes {
-    const val LOGIN = "login"
+    const val LOGIN = "login_screen"
+    const val TODO_LIST = "todo_list_screen"
 }
--- app/src/main/java/ch/kra/todo/core/UIEvent.kt
+++ app/src/main/java/ch/kra/todo/core/UIEvent.kt
@@ -6,5 +6,8 @@ sealed class UIEvent {
     data class ShowSnackbar(val message: String): UIEvent()
     object PopBackStack: UIEvent()
     data class Navigate(val route: String): UIEvent()
+    data class DisplayError(val error: String): UIEvent()
+    data class DisplaySuccess(val message: String): UIEvent()
+    object DisplayLoading: UIEvent()
     data class StartIntent(val intent: Intent): UIEvent()
 }
