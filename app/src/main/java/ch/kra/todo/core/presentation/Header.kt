ec4bc8b00799f1994daf22edabc0dda27b6c6c54
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2022-04-28 13:09:32 +0200

    Create the view for the todoList

--- app/src/main/java/ch/kra/todo/core/presentation/Header.kt
+++ app/src/main/java/ch/kra/todo/core/presentation/Header.kt
@@ -2,11 +2,16 @@ package ch.kra.todo.core.presentation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Header(
@@ -14,13 +19,20 @@ fun Header(
     username: String = ""
 ) {
     Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .background(MaterialTheme.colors.primary)
+            .padding(horizontal = 20.dp)
     ) {
         if (username.isNotEmpty()) {
             Text(
                 text = username,
-                textAlign = TextAlign.Right
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Right,
+                color = MaterialTheme.colors.onPrimary,
+                modifier = Modifier
+                    .weight(1f)
             )
         }
     }
--- app/src/main/java/ch/kra/todo/todo/presentation/todos/components/TodoListScreen.kt
+++ app/src/main/java/ch/kra/todo/todo/presentation/todos/components/TodoListScreen.kt
@@ -1,41 +1,60 @@
 package ch.kra.todo.todo.presentation.todos.components
 
+import android.os.Build
+import android.widget.Space
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ch.kra.todo.R
+import ch.kra.todo.core.DateFormatUtil
 import ch.kra.todo.core.UIEvent
 import ch.kra.todo.core.presentation.Footer
 import ch.kra.todo.core.presentation.Header
+import ch.kra.todo.core.presentation.LoadingWrapper
+import ch.kra.todo.core.presentation.TodoCard
+import ch.kra.todo.core.presentation.ui.theme.TextInfoColor
+import ch.kra.todo.todo.domain.model.Todo
+import ch.kra.todo.todo.presentation.todos.TodoListEvent
 import ch.kra.todo.todo.presentation.todos.TodoListViewModel
+import kotlinx.coroutines.flow.collect
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TodoListScreen(
     viewModel: TodoListViewModel = hiltViewModel(),
     navigate: (UIEvent.Navigate) -> Unit
 ) {
     val scaffoldState = rememberScaffoldState()
+    val username = viewModel.username.value
+    val todoListState = viewModel.todoListState.value
 
-    /*LaunchedEffect(key1 = true) {
+    LaunchedEffect(key1 = true) {
         viewModel.uiEvent.collect { event ->
             when (event) {
-                is UIEvent.Navigate -> navigate(event)
+                is UIEvent.Navigate -> {
+                    navigate(event)
+                }
 
-                is UIEvent.DisplayLoading -> {
-                    /* TODO */
+                is UIEvent.ShowSnackbar -> {
+
                 }
             }
         }
-    }*/
+    }
 
     Scaffold(
         bottomBar = {
@@ -58,15 +77,78 @@ fun TodoListScreen(
             Header(
                 modifier = Modifier
                     .height(46.dp)
-                    .fillMaxWidth()
+                    .fillMaxWidth(),
+                username = username
             )
             Text(
                 modifier = Modifier
                     .padding(20.dp),
-                text = stringResource(R.string.todo_list_header, "Kevin"),
+                text = stringResource(R.string.todo_list_header, username),
                 fontSize = 32.sp,
                 fontWeight = FontWeight.Bold
             )
+            LoadingWrapper(
+                modifier = Modifier.fillMaxSize(),
+                isLoading = todoListState.isLoading
+            ) {
+                TodoList(
+                    todoList = todoListState.todoList,
+                    onEvent = viewModel::onEvent
+                )
+            }
         }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+private fun TodoList(
+    todoList: List<Todo>,
+    onEvent: (TodoListEvent) -> Unit
+) {
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        items(todoList.size) {
+            TodoCard(
+                modifier = Modifier
+                    .clickable { onEvent(TodoListEvent.AddEditTodo(todoList[it].id ?: 0)) }
+            ) {
+                TodoItem(todo = todoList[it])
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+private fun TodoItem (
+    todo: Todo
+) {
+    Column {
+        Text(
+            text = todo.title,
+            textAlign = TextAlign.Center,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        
+        Spacer(modifier = Modifier.height(20.dp))
+        
+        Text(
+            text = stringResource(R.string.created_on, DateFormatUtil.formatToString(todo.createdAt)),
+            color = TextInfoColor
+        )
+        
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Text(
+            text = stringResource(R.string.last_updated_on, DateFormatUtil.formatToString(todo.lastUpdatedAt)),
+            color = TextInfoColor
+        )
     }
 }
