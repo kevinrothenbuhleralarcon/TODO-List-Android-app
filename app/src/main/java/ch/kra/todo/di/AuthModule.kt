c080bdb4be090e30c3ec04d75694edd08c393ef5
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2022-04-28 07:56:23 +0200

    Reorganise modules

--- app/src/main/java/ch/kra/todo/di/AuthModule.kt
+++ app/src/main/java/ch/kra/todo/di/AuthModule.kt
@@ -4,15 +4,28 @@ import ch.kra.todo.auth.data.remote.AuthApi
 import ch.kra.todo.auth.data.repository.AuthRepositoryImpl
 import ch.kra.todo.auth.domain.repository.AuthRepository
 import ch.kra.todo.auth.domain.use_case.Login
+import ch.kra.todo.core.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AuthModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthApi(): AuthApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AuthApi::class.java)
+    }
 
     @Provides
     @Singleton
--- app/src/main/java/ch/kra/todo/di/SharedModule.kt
+++ app/src/main/java/ch/kra/todo/di/SharedModule.kt
@@ -7,9 +7,7 @@ import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStoreFile
-import ch.kra.todo.auth.data.remote.AuthApi
-import ch.kra.todo.core.Constants.BASE_URL
-import ch.kra.todo.core.Constants.CONNECTION_PREFERENCE_NAME
+import ch.kra.todo.core.Constants
 import ch.kra.todo.core.data.local.SettingsDataStore
 import dagger.Module
 import dagger.Provides
@@ -19,13 +17,27 @@ import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object TodoModule {
+object SharedModule {
+    
+    @Provides
+    @Singleton
+    fun providePreferenceDataStore(@ApplicationContext appContext: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }
+            ),
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile = { appContext.preferencesDataStoreFile(Constants.CONNECTION_PREFERENCE_NAME) }
+        )
+    }
 
-
+    @Provides
+    @Singleton
+    fun provideSettingsDataStore(dataStore: DataStore<Preferences>): SettingsDataStore {
+        return SettingsDataStore(dataStore)
+    }
 }
