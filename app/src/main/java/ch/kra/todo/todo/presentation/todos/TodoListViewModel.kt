9534296991bd4c48843323495ea5671fcd83ae83
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2022-04-28 12:23:15 +0200

    Update the viewModel for the todoList Screen

--- app/src/main/java/ch/kra/todo/todo/presentation/todos/TodoListViewModel.kt
+++ app/src/main/java/ch/kra/todo/todo/presentation/todos/TodoListViewModel.kt
@@ -9,12 +9,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ch.kra.todo.core.DateFormatUtil
 import ch.kra.todo.core.Resource
+import ch.kra.todo.core.Routes
+import ch.kra.todo.core.UIEvent
 import ch.kra.todo.core.data.local.SettingsDataStore
 import ch.kra.todo.todo.domain.use_case.GetTodoList
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -29,9 +33,38 @@ class TodoListViewModel @Inject constructor(
     private val _username = mutableStateOf("")
     val username: State<String> = _username
 
+    private val _todoListState = mutableStateOf(TodoListState())
+    val todoListState: State<TodoListState> = _todoListState
+
+    private val _uiEvent = Channel<UIEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
     init {
         loadSettings()
         getTodoList()
+    }
+
+    fun onEvent(event: TodoListEvent) {
+        when (event) {
+            is TodoListEvent.AddEditTodo -> {
+                sendUIEvent(
+                    UIEvent.Navigate(
+                        Routes.ADD_EDIT_TODO +
+                                "/${event.todoId}"
+                    )
+                )
+            }
+
+            is TodoListEvent.Disconnect -> {
+                /* TODO */
+            }
+        }
+    }
+
+    private fun sendUIEvent(event: UIEvent) {
+        viewModelScope.launch {
+            _uiEvent.send(event)
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -41,15 +74,28 @@ class TodoListViewModel @Inject constructor(
                 .onEach { result ->
                 when (result) {
                     is Resource.Success -> {
-                        
+                        _todoListState.value = todoListState.value.copy(
+                            todoList = result.data ?: emptyList(),
+                            isLoading = false
+                        )
                     }
 
                     is Resource.Error -> {
-                        Log.d("todo", "error: ${result.message}")
+                        _todoListState.value = todoListState.value.copy(
+                            todoList = result.data ?: emptyList(),
+                            isLoading = false
+                        )
+                        /* TODO: Check if connection error and redirect */
+                        sendUIEvent(UIEvent.ShowSnackbar(
+                            result.message ?: "Unknown error"
+                        ))
                     }
 
                     is Resource.Loading -> {
-
+                        _todoListState.value = todoListState.value.copy(
+                            todoList = result.data ?: emptyList(),
+                            isLoading = true
+                        )
                     }
                 }
             }.launchIn(this)
