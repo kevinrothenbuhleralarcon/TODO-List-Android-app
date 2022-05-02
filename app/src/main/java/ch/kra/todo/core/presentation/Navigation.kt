643e1fe1410117ba63c6708b47a2e21ba0d7e768
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2022-05-02 08:42:20 +0200

    optimise import

--- app/src/main/java/ch/kra/todo/core/presentation/Navigation.kt
+++ app/src/main/java/ch/kra/todo/core/presentation/Navigation.kt
@@ -8,7 +8,6 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import androidx.navigation.navigation
 import ch.kra.todo.auth.presentation.login.components.LoginScreen
 import ch.kra.todo.core.Routes
 import ch.kra.todo.todo.presentation.add_edit_todo.components.AddEditScreen
